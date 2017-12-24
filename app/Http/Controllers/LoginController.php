2b00b1abe88d133babdfbd8a8b15351446abb798
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2017-12-23 22:49:04 -0200

    release login and logout with release create new user

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace estoque\Http\Controllers;
+
+use Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class LoginController extends Controller
+{
+    public function __construct(){
+
+      $this->middleware('autorizador');
+    }
+    public function form(){
+    	return view('form_login');
+    }
+    public function login(){
+    $credenciais = Request::only('email','password');
+
+     if(!Auth::attempt(['email' => $credenciais['email'], 'password' => $credenciais['password']])) {
+        return "Usuário ". 
+            Auth::user()->name 
+            ." logado com sucesso";
+            }
+            return 'Não Logou';
+     }
+
+    public function logout(){
+        Auth::logout();
+    	return'deslogou';
+    }
+}
